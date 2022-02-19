dc4944046f4b3e9a97ebe6bdc29ff61fd8dedb1e
ogbutec <ing.orlandogiron@gmail.com>
2022-02-19 13:25:31 -0600

    PreParcial

---
+++ routerflutter/lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(AppRoute());
+}
+
+class AppRoute extends StatelessWidget {
+  const AppRoute({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Mi aplicacion',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Nombre de mi app"),
+        ),
+        body: Center(
+          child: Container(
+            child: Text(
+              "Practica previa a examen",
+              style: TextStyle(fontSize: 25, fontFamily: 'Roboto'),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
