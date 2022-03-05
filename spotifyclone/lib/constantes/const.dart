096df3203fde59e00f766576f14185f0f5a8bad5
ogbutec <ing.orlandogiron@gmail.com>
2022-03-05 14:19:47 -0600

    envio

---
+++ spotifyclone/lib/constantes/const.dart
@@ -0,0 +1,38 @@
+import 'package:spotifyclone/paginas/principal_app.dart';
+
+const List song_type_1 = [
+  "Music",
+  "Rock",
+  "Soul",
+  "Classic",
+  "Pop",
+  "Reggae",
+  "Indie"
+];
+
+const List song_type_2 = [
+  "Podcast",
+  "Hecho para usted",
+  "Influencias",
+  "Conciertos",
+  "Lanzacmientos"
+];
+
+const List song = [
+  {
+    "img": "assets/images/img3.jpg",
+    "title": "La bala",
+    "descriptions": "Los hermanos Flores",
+    "song_count": "100 canciones",
+    "date": "hace 19hr",
+    "song_url": "song/1.mp3",
+    "songs": [
+      {"title": "El churro numero 2", "duration": "1.20"},
+      {
+        "title": "El churro numero 2",
+        "duration": "1.20",
+        "portada": "asstes/img/1.jpg"
+      }
+    ]
+  }
+];
--- spotifyclone/lib/paginas/home.dart
+++ spotifyclone/lib/paginas/home.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_feather_icons/flutter_feather_icons.dart';
 import 'package:flutter_vector_icons/flutter_vector_icons.dart';
 
+import '../constantes/const.dart';
+
 class PaginaPrincipal extends StatefulWidget {
   @override
   State<PaginaPrincipal> createState() => _PaginaPrincipalState();
@@ -12,7 +14,10 @@ class _PaginaPrincipalState extends State<PaginaPrincipal> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      appBar: AppBar(title: mostrarAppBar()),
+      appBar: AppBar(
+        title: mostrarAppBar(),
+      ),
+      body: bodyAppSpf(),
       //: mostrarAppBar(),
     );
   }
@@ -21,16 +26,55 @@ class _PaginaPrincipalState extends State<PaginaPrincipal> {
     return AppBar(
       backgroundColor: Colors.black,
       elevation: 0,
-      title: Row(
-        children: [
-          Text(
-            "Explorar",
-            style: TextStyle(
-                fontSize: 20, color: Colors.white, fontWeight: FontWeight.bold),
-          ),
-          Icon(Entypo.list),
-        ],
+      title: Padding(
+        padding: const EdgeInsets.only(left: 10, right: 10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              "Explorar",
+              style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold),
+            ),
+            Icon(Entypo.list),
+          ],
+        ),
       ),
     );
+  }
+
+  Widget bodyAppSpf() {
+    return SingleChildScrollView(
+        child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          //crossAxisAlignment: CrossAxisAlignment.start,
+          child: Padding(
+            padding: const EdgeInsets.only(left: 30, top: 20),
+            child: Row(
+                children: List.generate(song_type_1.length, (index) {
+              return Padding(
+                padding: const EdgeInsets.only(right: 25),
+                child: Column(
+                  children: [
+                    Text(
+                      song_type_1[index],
+                      style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 15,
+                          fontWeight: FontWeight.w600),
+                    )
+                  ],
+                ),
+              );
+            })),
+          ),
+        )
+      ],
+    ));
   }
 }
